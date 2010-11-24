6ace0b91e9c577843eb64260e63b7e10708001b5
holtonma <holtonma@gmail.com>
2010-11-23 18:22:06 -0800

    first Roles admin and author; user methods to set, get, plus named scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,27 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable, :lockable and :timeoutable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation 
-  #:remember_me
+  # accessible attributes for your model
+  attr_accessible :email, :password, :password_confirmation #:remember_me
+  
+  named_scope :with_role, lambda { |role| {:conditions => "roles_mask & #{2**ROLES.index(role.to_s)} > 0 "}}
+  
+  ROLES = %w[admin author]
+  
+  def role_symbols
+    roles.map(&:to_sym)
+  end
+  
+  def roles=(roles)
+    self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.sum
+  end
+  
+  def roles
+    ROLES.reject { |r| ((roles_mask || 0) & 2**ROLES.index(r)).zero? }
+  end
+  
+  
+  
 end
